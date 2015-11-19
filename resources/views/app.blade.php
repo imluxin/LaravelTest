1c8cfe5e02736ade62165afd97b2e6524b222b77
Luxin <luxin.yao@hotmail.com>
2015-11-19 10:56:02 +0800

    新增app.blade.php, blade模板学习

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.css">
+    <script type="text/javascript" src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
--- resources/views/cv/index.blade.php
+++ resources/views/cv/index.blade.php
@@ -1,14 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>MY CV</title>
-</head>
-<body>
-    <h1>My CV</h1>
+@extends('app')
+
+@section('content')
+    <h1>About me</h1>
     <p>
         name: {{ $name }} <br>
         age: {{ $age }}
     </p>
-</body>
-</html>
+@stop
