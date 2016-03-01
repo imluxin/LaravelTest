802a4597c4f84d4523e65f7173e43b81f2f123da
Lux <43660254@qq.com>
2016-03-01 22:43:09 +0800

    composer update

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,15 +6,16 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'ee65fd8033b69a16866fce8167c9aa34' => $vendorDir . '/hamcrest/hamcrest-php/hamcrest/Hamcrest.php',
-    '6fac04860a2cf4b03e356f4caca377d6' => $vendorDir . '/symfony/var-dumper/Resources/functions/dump.php',
-    '742cc5b936ec765a3455c0a976796f29' => $vendorDir . '/nikic/php-parser/lib/bootstrap.php',
-    '01b872096dad055849387e42d41a176d' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
-    '45832374d4ddfd01a421783a590f8253' => $vendorDir . '/psy/psysh/src/Psy/functions.php',
-    '64777cd7d356cde6e827c1fa49f98dbd' => $vendorDir . '/paragonie/random_compat/lib/random.php',
     '9b24cdf6705c981dc519077514baadf4' => $vendorDir . '/danielstjules/stringy/src/Create.php',
-    '8228127a88ff67cb5daa8ca7907cf435' => $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
-    '9bfc7149f9758d59054ff388ac6d30d2' => $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
+    '6fac04860a2cf4b03e356f4caca377d6' => $vendorDir . '/symfony/var-dumper/Resources/functions/dump.php',
+    'ee65fd8033b69a16866fce8167c9aa34' => $vendorDir . '/hamcrest/hamcrest-php/hamcrest/Hamcrest.php',
+    '35c543157d8c3a25ba4207ce9aeaf263' => $vendorDir . '/illuminate/support/helpers.php',
+    '01b872096dad055849387e42d41a176d' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
+    '03d83d8496ed2ba16ae326b097a7a1a9' => $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
+    '94d93ee914223d222bc4b0bf6115abed' => $vendorDir . '/psy/psysh/src/Psy/functions.php',
+    'c842b1fe0ca47d69d520e2d8091024b3' => $vendorDir . '/paragonie/random_compat/lib/random.php',
     '8c9698b305e308ed34201edf1f3c1e48' => $vendorDir . '/illuminate/html/helpers.php',
     '8ab8815013ff924942023cfe80d8a978' => $vendorDir . '/laracasts/flash/src/Laracasts/Flash/functions.php',
+    '9a8f7f680e9a961c6784d917e19535d4' => $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
+    'd0c927991b38659f7eaab6618d705a95' => $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
 );
